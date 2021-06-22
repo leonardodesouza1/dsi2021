b1af80ad70434ce995b82db890224a475bd43097
leonardodesouza1 <leonardodesouza2108@gmail.com>
2021-06-22 01:24:13 +0000

    Customizações Produto

--- src/main/java/br/univille/leonardosouzadsi2021/controller/ProdutoController.java
+++ src/main/java/br/univille/leonardosouzadsi2021/controller/ProdutoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,5 +24,17 @@ public class ProdutoController {
     public ModelAndView index(){
         List<Produto> produtos = service.getAll();
         return new ModelAndView("produto/index","produtos", produtos);
+    }
+
+    @GetMapping("/cadastro")
+    public ModelAndView cadastro(@ModelAttribute Produto produto){
+        List<Produto> produtos = service.getAll();
+        return new ModelAndView("produto/form","produtos", produtos);
+    }
+
+    @PostMapping(params="form")
+    public ModelAndView save(Produto produto){
+        System.out.println(produto.getNome());
+        return new ModelAndView("produto/form");
     }
 }
